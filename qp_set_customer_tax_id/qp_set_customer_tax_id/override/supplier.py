22fa252baa3767f6157c53520c59775548665770
afuentes-alphas <afuentes.mentum@gmail.com>
2023-05-18 13:37:02 -0400

    [FIX] Se incorpora un import faltante para el nombramiento del proveedor.

--- qp_set_customer_tax_id/qp_set_customer_tax_id/override/supplier.py
+++ qp_set_customer_tax_id/qp_set_customer_tax_id/override/supplier.py
@@ -1,13 +1,13 @@
 import re
 import frappe
 from frappe import _
+from frappe.model.naming import set_name_by_naming_series
 from erpnext.buying.doctype.supplier.supplier import Supplier
 
 
 class QlipSupplier(Supplier):
 
     def autoname(self):
-        print("--------------------qlip---------------------")
         supp_master_name = frappe.defaults.get_global_default('supp_master_name')
         if supp_master_name == 'Supplier Name':
             self.name = self.get_supplier_name()
