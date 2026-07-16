e11e728f3d0e4794be6cc2cdfa1af1acedfef26f
afuentes-alphas <afuentes.mentum@gmail.com>
2026-07-16 18:09:43 +0000

    [MOD] Validar duplicado de tax_id: Ajuste en indentación.

--- qp_set_customer_tax_id/qp_set_customer_tax_id/uses_cases/validation.py
+++ qp_set_customer_tax_id/qp_set_customer_tax_id/uses_cases/validation.py
@@ -6,11 +6,10 @@ def handle(documento, method):
 
     # Garantizar que el tax_id no se repita
 
-        duplicado = frappe.db.sql("""
-		select tax_id from tab{0}
-        where tax_id = {1}
-        and name != {2}""".format(documento.doctype, documento.tax_id, documento.name), as_dict=True)
+    duplicado = frappe.db.sql("""
+    select tax_id from tab{0}
+    where tax_id = {1}
+    and name != {2}""".format(documento.doctype, documento.tax_id, documento.name), as_dict=True)
 
-        print("duplicado", duplicado)
-        if duplicado:
-            frappe.throw(_("Duplicate {0} found in the table").format(documento.tax_id))
+    if duplicado:
+        frappe.throw(_("Duplicate {0} found in the table").format(documento.tax_id))
